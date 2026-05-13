34f017ff27bbdf771002de157e8731c1cbb2bf43
walter1243 <walter1243@users.noreply.github.com>
2026-05-13 18:04:32 -0300

    fix: adicionar DELETE ao CORS, melhorar impressao e preparar env DELETE_PASSWORD

--- api/_utils.js
+++ api/_utils.js
@@ -23,7 +23,7 @@ function getSql() {
 
 function setCorsHeaders(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 }
 
